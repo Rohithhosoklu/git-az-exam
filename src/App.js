9fffdb5e3be175ded415fe74286ba7bcaa1d64cf
BhavishyaRamesh <bhavishyaramesh02@gmail.com>
2022-05-19 17:44:29 +0530

    Updated

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         
-        <h1>Git to Azure</h1>
+        <h1>hello</h1>
         <a
           className="App-link"
           href="https://reactjs.org"
