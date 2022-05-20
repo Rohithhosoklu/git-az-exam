bb3885385d8d80af3dc6567976744fd27e72b7c2
Rohithhosoklu <86466698+Rohithhosoklu@users.noreply.github.com>
2022-05-20 10:21:51 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,14 +7,14 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         
-        <h1>hello</h1>
+        <h1>hello world</h1>
         <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+         
         </a>
       </header>
     </div>
